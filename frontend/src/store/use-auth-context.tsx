82fe82f898c88270b0e855b3828601a4928a2c05
Alschn <67019937+Alschn@users.noreply.github.com>
2024-05-26 16:45:56 +0200

    Remove old auth context related code

--- frontend/src/store/use-auth-context.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useContext } from "react";
-import { AuthContext } from "~/store/auth-context";
-
-export default function useAuthContext() {
-  return useContext(AuthContext);
-}
