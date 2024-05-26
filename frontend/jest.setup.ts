66906c07140ac317d9ca0dd1a6023c35ec43d107
Alschn <67019937+Alschn@users.noreply.github.com>
2024-05-26 17:22:34 +0200

    Mock env.js in jest tests, remove SKIP_ENV_VALIDATION from workflows

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+
+// env file has to be mocked because Jest is stupid and does not fully support ES6 modules
+jest.mock("~/env.js", () => ({
+  env: {
+    NEXT_PUBLIC_API_URL: "http://127.0.0.1:8000",
+  },
+}));
